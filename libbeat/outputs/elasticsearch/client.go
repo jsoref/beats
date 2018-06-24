895a299e10a461b797ca9d5d51e930b9cfb9e89d
Josh Soref <jsoref@users.noreply.github.com>
2018-06-24 19:58:06 +0000

    spelling: response

--- libbeat/outputs/elasticsearch/client.go
+++ libbeat/outputs/elasticsearch/client.go
@@ -462,7 +462,7 @@ func bulkCollectPublishFails(
 	data []publisher.Event,
 ) ([]publisher.Event, bulkResultStats) {
 	if err := reader.expectDict(); err != nil {
-		logp.Err("Failed to parse bulk respose: expected JSON object")
+		logp.Err("Failed to parse bulk response: expected JSON object")
 		return nil, bulkResultStats{}
 	}
 
@@ -489,7 +489,7 @@ func bulkCollectPublishFails(
 
 	// check items field is an array
 	if err := reader.expectArray(); err != nil {
-		logp.Err("Failed to parse bulk respose: expected items array")
+		logp.Err("Failed to parse bulk response: expected items array")
 		return nil, bulkResultStats{}
 	}
 
